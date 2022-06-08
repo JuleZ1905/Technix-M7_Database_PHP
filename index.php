64a28d15b9c969b5976348e45ace3424ca518189
JuleZ_2k4 <8096@htl.rennweg.at>
2022-06-08 17:40:57 +0200

    changes made

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 <?php
 
-$html = file_get_contents(__DIR__ . '/Templates/index.html');
+$html = file_get_contents('/Templates/index.html');
+
+/**
+ * data which needs to be sent to the template
+ * 1) playernames
+ * 2) playerids
+ * 3) gameround
+ * 4) date
+ * 5) winner
+ * 6) symbols picked
+ */
+
+
 
 echo $html;
 
--- lib/Database/database.sql
+++ lib/Database/database.sql
@@ -5,21 +5,32 @@ CREATE DATABASE IF NOT EXISTS tournament DEFAULT CHARACTER SET utf8 DEFAULT COLL
 USE tournament;
 
 DROP TABLE IF EXISTS player;
-CREATE TABLE player (
-    pk_player_id INTEGER PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL
+CREATE TABLE player
+(
+    pk_player_id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    first_name   VARCHAR(50) NOT NULL,
+    last_name    VARCHAR(50) NOT NULL
 );
 
 DROP TABLE IF EXISTS gameround;
-CREATE TABLE gameround (
+CREATE TABLE gameround
+(
     pk_round_id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    `date` DATE NOT NULL,
-    `time` TIME NOT NULL,
-    fk_p1 INT NOT NULL,
-    fk_p2 INT NOT NULL,
-    symbol_p1 VARCHAR(15) NOT NULL,
-    symbol_p2 VARCHAR(15) NOT NULL,
-    CONSTRAINT fk_gameround_p1 FOREIGN KEY (fk_p1) REFERENCES player(pk_player_id),
-    CONSTRAINT fk_gameround_p2 FOREIGN KEY (fk_p2) REFERENCES player(pk_player_id)
+    `date`      DATE            NOT NULL,
+    `time`      TIME            NOT NULL,
+    fk_p1       INT             NOT NULL,
+    fk_p2       INT             NOT NULL,
+    symbol_p1   VARCHAR(15)     NOT NULL,
+    symbol_p2   VARCHAR(15)     NOT NULL,
+    CONSTRAINT fk_gameround_p1 FOREIGN KEY (fk_p1) REFERENCES player (pk_player_id),
+    CONSTRAINT fk_gameround_p2 FOREIGN KEY (fk_p2) REFERENCES player (pk_player_id)
 );
+
+INSERT INTO player (first_name, last_name)
+VALUES ('Julian', 'Zangl'),
+       ('Katharina', 'Jaros'),
+       ('Corvin', 'Prath'),
+       ('Bernhard', 'Myska');
+
+
+
